3f042ebd60c55b18c42e482d94a8184c18356906
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-05-31 23:56:34 +0200

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import ReactDom from "react-dom"
 import Navbar from "./components/navbar"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Product from "./components/product";
 
 // const element = <h1>Hello World</h1>;
 
-ReactDom.render(<Navbar />, document.getElementById('root'));
+ReactDom.render(<Product />, document.getElementById('root'));
